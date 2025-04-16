8112a9e91db58d008d3813c29cc31a545f75da3c
Viktoryia <viktoriabakun00@gmail.com>
2025-04-16 12:32:21 +0200

    feat: Text component
    Signed-off-by: Viktoryia <viktoriabakun00@gmail.com>

---
+++ src/lib/typography/Text.svelte
@@ -0,0 +1,32 @@
+<script>
+	/** @typedef {'p' | 'span' | 'div' | 'label' | 'strong' | 'em' | 'small' | 'blockquote' | 'code' | 'mark' | 'time'} TextHtmlElement */
+
+	/** @type {{
+	 * tag?: TextHtmlElement | string,
+	 * size?: 'display-2xl' | 'display-md' | 'xl' | 'lg' | 'md',
+	 * weight?: 'regular' | 'medium' | 'semibold' | 'bold',
+	 * children: () => any,
+	 * class?: string,
+	 * }} */
+
+	let { tag = 'p', size = 'md', weight = 'regular', children, ...restProps } = $props();
+
+	const sizeClasses = {
+		'display-2xl': 'text-[72px] leading-tight',
+		'display-md': 'text-[36px] leading-snug',
+		xl: 'text-[20px] leading-relaxed',
+		lg: 'text-[18px] leading-relaxed',
+		md: 'text-[16px] leading-relaxed'
+	};
+
+	const weightClasses = {
+		regular: 'font-normal',
+		medium: 'font-medium',
+		semibold: 'font-semibold',
+		bold: 'font-bold'
+	};
+</script>
+
+<svelte:element this={tag} class={[sizeClasses[size], weightClasses[weight], restProps.class]}>
+	{@render children()}
+</svelte:element>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,5 @@
-<div>Hello World</div>
+<script>
+	import Text from '$lib/typography/Text.svelte';
+</script>
+
+<Text size="display-2xl">Hello world</Text>
