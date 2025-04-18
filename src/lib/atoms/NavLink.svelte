b4e28ae7946e3c0513524e67c0e24b1c9b630a37
Uladzimir Shautsou <shevv920@proton.me>
2025-04-18 22:08:00 +0200

    feat: add nav links - WIP

---
+++ src/lib/atoms/NavLink.svelte
@@ -0,0 +1,16 @@
+<script>
+
+	/**
+	 * @type {{ title: string, target: string }}
+	 */
+	const { title, target } = $props();
+	import { page } from "$app/state";
+</script>
+
+<a
+	href={target}
+	class="text-md text-white/40 items-center flex hover:text-white [&[aria-current='page']]:text-white/80 navLinkDot"
+	aria-current={page.url.hash === target ? 'page': undefined}
+>
+	{title}
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,32 @@
 <script>
 	import Text from '$lib/typography/Text.svelte';
+	import NavLink from '$lib/atoms/NavLink.svelte';
 </script>
 
 <Text>Hello World</Text>
+
+	<nav class="fixed top-0 flex gap-2 w-full bg-black">
+	<NavLink title="Home" target="#" />
+	<NavLink title="About" target="#about" />
+	<NavLink title="Skills" target="#skills" />
+	<NavLink title="Works" target="#works" />
+	<NavLink title="Contact" target="#contact" />
+	</nav>
+
+<div class="h-[200px] bg-pink-200">blabla</div>
+
+<section id="about" class="target:bg-green-200">
+	about lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit amet
+</section>
+
+<section id="skills" class="target:bg-green-300">
+	skills lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit amet
+</section>
+
+<section id="works" class="target:bg-green-400">
+	works lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit amet
+</section>
+
+<section id="contact" class="target:bg-green-500">
+	contact lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit ametabout lorem ipsum dolor sit amet
+</section>
