3b9eed460f4cbf06715d65be099073903411ac60
Uladzimir Shautsou <442126+shevv920@users.noreply.github.com>
2025-04-21 21:13:23 +0200

    feat: navigation

---
+++ src/lib/atoms/NavLink.svelte
@@ -0,0 +1,15 @@
+<script>
+	/**
+	 * @type {{ title: string, target: string }}
+	 */
+	const { title, target } = $props();
+	import { page } from '$app/state';
+</script>
+
+<a
+	href={target}
+	class="navLinkDot text-md flex items-center py-2 pr-3 pl-[30px] text-white/40 transition-colors duration-400 hover:text-white [&[aria-current='page']]:text-white/80"
+	aria-current={page.url.hash === target ? 'page' : undefined}
+>
+	{title}
+</a>
