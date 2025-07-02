0c80a4c1cb427da3177f8cde96e7c9a8b6354253
Viktoryia <viktoriabakun00@gmail.com>
2025-07-02 13:06:05 +0200

    feat: reviews
    Signed-off-by: Viktoryia <viktoriabakun00@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,14 @@
 	import About from '$lib/components/sections/About.svelte';
 	import Works from '$lib/components/sections/Works.svelte';
 	import Skills from '$lib/components/sections/Skills.svelte';
+	import Reviews from '$lib/components/sections/Reviews.svelte';
 </script>
 
 <Hero />
 <About />
 <Works />
 <Skills />
+<Reviews />
 
 <section class="section-scroll-margin max-container" id="contact">
 	<h2 class="text-6xl text-pink-300 lg:text-9xl">Contact</h2>
