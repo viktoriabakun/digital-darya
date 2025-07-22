6b88846a5c686a3306d673317294c71133b080df
Viktoryia <viktoriabakun00@gmail.com>
2025-07-22 12:41:21 +0200

    chore: remove unused code
    Signed-off-by: Viktoryia <viktoriabakun00@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,67 +14,6 @@
 <Reviews />
 <Experience />
 
-<!--<section class="section-scroll-margin max-container" id="contact">-->
-<!--	<h2 class="text-6xl text-pink-300 lg:text-9xl">Contact</h2>-->
-<!--	<Text>-->
-<!--		contact _____ Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae. Dolorum,-->
-<!--		quod. Molestias voluptatum eveniet, debitis repellat quae rerum quibusdam? Voluptate? Lorem-->
-<!--		ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae. Dolorum, quod. Molestias-->
-<!--		voluptatum eveniet, debitis repellat quae rerum quibusdam? Voluptate? Lorem ipsum dolor sit amet-->
-<!--		consectetur adipisicing elit. Quisquam, quae. Dolorum, quod. Molestias voluptatum eveniet,-->
-<!--		debitis repellat quae rerum quibusdam? Voluptate? Lorem ipsum dolor sit amet consectetur-->
-<!--		adipisicing elit. Quisquam, quae. Dolorum, quod. Molestias voluptatum eveniet, debitis repellat-->
-<!--		quae rerum quibusdam? Voluptate? Lorem ipsum dolor sit amet consectetur adipisicing elit.-->
-<!--		Quisquam, quae. Dolorum, quod. Molestias voluptatum eveniet, debitis repellat quae rerum-->
-<!--		quibusdam? Voluptate? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.-->
-<!--		Dolorum, quod. Molestias voluptatum eveniet, debitis repellat quae rerum quibusdam? Voluptate?-->
-<!--		Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae. Dolorum, quod.-->
-<!--		Molestias voluptatum eveniet, debitis repellat quae rerum quibusdam? Voluptate? Lorem ipsum-->
-<!--		dolor sit amet consectetur adipisicing elit. Quisquam, quae. Dolorum, quod. Molestias voluptatum-->
-<!--		eveniet, debitis repellat quae rerum quibusdam? Voluptate? Lorem ipsum dolor sit amet-->
-<!--		consectetur adipisicing elit. Quisquam, quae. Dolorum, quod. Molestias voluptatum eveniet,-->
-<!--		debitis repellat quae rerum quibusdam? Voluptate? Lorem ipsum dolor sit amet consectetur-->
-<!--		adipisicing elit. Quisquam, quae. Dolorum, quod. Molestias voluptatum eveniet, debitis repellat-->
-<!--		quae rerum quibusdam? Voluptate? Lorem ipsum dolor sit amet consectetur adipisicing elit.-->
-<!--		Quisquam, quae. Dolorum, quod. Molestias voluptatum eveniet, debitis repellat quae rerum-->
-<!--		quibusdam? Voluptate? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.-->
-<!--		Dolorum, quod. Molestias voluptatum eveniet, debitis repellat quae rerum quibusdam? Voluptate?-->
-<!--		Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae. Dolorum, quod.-->
-<!--		Molestias voluptatum eveniet, debitis repellat quae rerum quibusdam? Voluptate? Lorem ipsum-->
-<!--		dolor sit amet consectetur adipisicing elit. Quisquam, quae. Dolorum, quod. Molestias voluptatum-->
-<!--		eveniet, debitis repellat quae rerum quibusdam? Voluptate? Lorem ipsum dolor sit amet-->
-<!--		consectetur adipisicing elit. Quisquam, quae. Dolorum, quod. Molestias voluptatum eveniet,-->
-<!--		debitis repellat quae rerum quibusdam? Voluptate? Lorem ipsum dolor sit amet consectetur-->
-<!--		adipisicing elit. Quisquam, quae. Dolorum, quod. Molestias voluptatum eveniet, debitis repellat-->
-<!--		quae rerum quibusdam? Voluptate? Lorem ipsum dolor sit amet consectetur adipisicing elit.-->
-<!--		Quisquam, quae. Dolorum, quod. Molestias voluptatum eveniet, debitis repellat quae rerum-->
-<!--		quibusdam? Voluptate? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.-->
-<!--		Dolorum, quod. Molestias voluptatum eveniet, debitis repellat quae rerum quibusdam? Voluptate?-->
-<!--		Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae. Dolorum, quod.-->
-<!--		Molestias voluptatum eveniet, debitis repellat quae rerum quibusdam? Voluptate? Lorem ipsum-->
-<!--		dolor sit amet consectetur adipisicing elit. Quisquam, quae. Dolorum, quod. Molestias voluptatum-->
-<!--		eveniet, debitis repellat quae rerum quibusdam? Voluptate? Lorem ipsum dolor sit amet-->
-<!--		consectetur adipisicing elit. Quisquam, quae. Dolorum, quod. Molestias voluptatum eveniet,-->
-<!--		debitis repellat quae rerum quibusdam? Voluptate? Lorem ipsum dolor sit amet consectetur-->
-<!--		adipisicing elit. Quisquam, quae. Dolorum, quod. Molestias voluptatum eveniet, debitis repellat-->
-<!--		quae rerum quibusdam? Voluptate? Lorem ipsum dolor sit amet consectetur adipisicing elit.-->
-<!--		Quisquam, quae. Dolorum, quod. Molestias voluptatum eveniet, debitis repellat quae rerum-->
-<!--		quibusdam? Voluptate? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.-->
-<!--		Dolorum, quod. Molestias voluptatum eveniet, debitis repellat quae rerum quibusdam? Voluptate?-->
-<!--		Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae. Dolorum, quod.-->
-<!--		Molestias voluptatum eveniet, debitis repellat quae rerum quibusdam? Voluptate? Lorem ipsum-->
-<!--		dolor sit amet consectetur adipisicing elit. Quisquam, quae. Dolorum, quod. Molestias voluptatum-->
-<!--		eveniet, debitis repellat quae rerum quibusdam? Voluptate? Lorem ipsum dolor sit amet-->
-<!--		consectetur adipisicing elit. Quisquam, quae. Dolorum, quod. Molestias voluptatum eveniet,-->
-<!--		debitis repellat quae rerum quibusdam? Voluptate? Lorem ipsum dolor sit amet consectetur-->
-<!--		adipisicing elit. Quisquam, quae. Dolorum, quod. Molestias voluptatum eveniet, debitis repellat-->
-<!--		quae rerum quibusdam? Voluptate? Lorem ipsum dolor sit amet consectetur adipisicing elit.-->
-<!--		Quisquam, quae. Dolorum, quod. Molestias voluptatum eveniet, debitis repellat quae rerum-->
-<!--		quibusdam? Voluptate? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.-->
-<!--		Dolorum, quod. Molestias voluptatum eveniet, debitis repellat quae rerum quibusdam? Voluptate?-->
-<!--	</Text>-->
-<!--</section>-->
-
 <style>
 	section {
 		border: 1px solid gray;
