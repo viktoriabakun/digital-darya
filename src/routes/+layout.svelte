5007767794dd38ab9c4e3ad6649066873784e76c
Viktoryia <viktoriabakun00@gmail.com>
2025-09-19 18:04:14 +0300

    try ignoreMobileResize
    Signed-off-by: Viktoryia <viktoriabakun00@gmail.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,17 @@
 <script>
-	import '../app.css';
 	import Header from '$lib/components/organisms/Header.svelte';
 	import Footer from '$lib/components/organisms/Footer.svelte';
+	import gsap from 'gsap';
+	import { ScrollTrigger } from 'gsap/ScrollTrigger';
+	import { onMount } from 'svelte';
+	import '../app.css';
 
 	let { children } = $props();
+
+	onMount(() => {
+		gsap.registerPlugin(ScrollTrigger);
+		ScrollTrigger.config({ ignoreMobileResize: true });
+	});
 </script>
 
 <Header />
